0a35865390ffe91cdda879a6a3f4c788f159c893
Raphaël Oester <raphaeloester@gmail.com>
2024-11-12 13:49:26 +0200

    delete legacy app1

--- src/AppV2.tsx
+++
@@ -1,6 +0,0 @@
-import './App.css'
-import App from "./app/App.tsx";
-
-export default function AppTwo() {
-    return <App/>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,17 @@
 // import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import AppTwo from "./AppV2.tsx";
-import AppOne from "./AppV1.tsx";
 
 import {
     createBrowserRouter,
     RouterProvider,
 } from 'react-router-dom';
 
+import App from "./app/App.tsx";
 
 const router = createBrowserRouter([{
-    path: "v1",
-    element: <AppOne/>,
-}, {
-    path: "v2",
-    element: <AppTwo/>
+    path: "",
+    element: <App/>
 },])
 
 createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/>);
