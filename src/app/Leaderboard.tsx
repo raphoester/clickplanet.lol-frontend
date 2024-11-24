9fecf2ac76ecf40d34619eda9c15246b14651519
Raphaël Oester <raphaeloester@gmail.com>
2024-11-24 11:59:49 +0200

    remove test data and add index in leaderboard

--- src/app/Leaderboard.tsx
+++ src/app/Leaderboard.tsx
@@ -10,19 +10,13 @@ type LeaderboardProps = {
 }
 
 export default function Leaderboard(props: LeaderboardProps) {
-    if (props.data.length == 0) {
-        props.data.push(
-            {country: {name: "Bulgaria", code: "bg"}, tiles: 1000},
-            {country: {name: "France", code: "fr"}, tiles: 500},
-            {country: {name: "British Indian Ocean Territory", code: "io"}, tiles: 300},
-        )
-    }
     return <div className="leaderboard">
         <h2 className="leaderboard-title">Leaderboard</h2>
         <table>
             <tbody>
             {props.data.map((entry, index) => {
                 return <tr key={index} className="leaderboard-entry">
+                    <td>{index + 1}</td>
                     <td>{(() => {
                         const sliced = entry.country.name.slice(0, 20)
                         return sliced.length === 20 ? sliced + "..." : sliced
