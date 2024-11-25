f44526dd968cbb8f833bc225140ec9aa7020ac4f
Raphaël Oester <raphaeloester@gmail.com>
2024-11-25 09:34:40 +0200

    yeet mobile blocker, add notice for mobile users

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,24 @@
 import Viewer, {ViewerProps} from "./viewer/Viewer.tsx";
 
-// import "./App.css"
-import MobileDeviceBlocker from "./MobileDeviceBlocker.tsx";
 import Warning from "./Warning.tsx";
 
 export type AppProps = ViewerProps
 
 export default function App(props: AppProps) {
-    if (detectMobile()) {
-        return <MobileDeviceBlocker/>
-    }
 
     return <>
-        <Warning
-            title={"HIGH LOAD NOTICE"}
+        {detectMobile() && <Warning
+            title={"MOBILE VERSION WARNING"}
             messages={[
-                `The site currently holds a lot of data.`,
-                `When the page loads, NO countries are displayed on the map.`,
-                `They appear after around a minute, depending on your connection.`,
-                `I'm working on the issue, thanks for your patience.`]}
-        />
+                `This site has been designed and is optimized for computers.`,
+                `The mobile version is being implemented, but it is not finished yet. 
+                You are free to use it, but it may not work as expected.`,
+                `In particular, the initial loading of the map might take a while, and contain a lot of glitches.`,
+                `The leaderboard is also pretty ugly and takes too much space.`,
+                `Thanks for your understanding.`
+            ]}
+        />}
+
         <Viewer
             tileClicker={props.tileClicker}
             ownershipsGetter={props.ownershipsGetter}
