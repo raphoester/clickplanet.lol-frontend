248abc0f2955932cfe133f751ac80972bc4e0095
Raphaël Oester <raphaeloester@gmail.com>
2024-11-25 17:41:11 +0200

    fix html semantics in leaderboard and collapse on click

--- src/app/Leaderboard.tsx
+++ src/app/Leaderboard.tsx
@@ -1,5 +1,6 @@
 import "./Leaderboard.css"
 import {Country} from "./countries.ts";
+import {useState} from "react";
 
 
 type LeaderboardProps = {
@@ -10,31 +11,39 @@ type LeaderboardProps = {
 }
 
 export default function Leaderboard(props: LeaderboardProps) {
+    const [isOpen, setIsOpen] = useState(true)
+    const toggleLeaderboard = () => {
+        setIsOpen(!isOpen)
+    }
+
     return <div className="leaderboard clean-scrollbar">
-        <h2 className="leaderboard-title">Leaderboard</h2>
-        <table>
+        <div className="leaderboard-header" onClick={toggleLeaderboard}>
+            <h2 className="leaderboard-title">Leaderboard</h2>
+            <p className="leaderboard-caption">(click to {isOpen ? "collapse" : "expand"})</p>
+        </div>
+        {isOpen && <table>
             <tbody>
             {props.data.map((entry, index) => {
                 return <tr key={index} className="leaderboard-entry">
-                            <div>
-                                <td>{index + 1}</td>
-                                <td><img
-                                    className="leaderboard-entry-flag"
-                                    src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
-                                    alt={`flag of ${entry.country.name}`}
-                                    title={entry.country.name}
-                                />
-                                </td>
-                                <td>{(() => {
-                                    const sliced = entry.country.name.slice(0, 20)
-                                    return sliced.length === 20 ? sliced + "..." : sliced
-                                })()}</td>
-                            </div>
-
-                            <td className="leaderboard-tiles">{entry.tiles}</td>
-                        </tr>
+                    <td>
+                        <span>{index + 1}</span>
+                        <span><img
+                            className="leaderboard-entry-flag"
+                            src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
+                            alt={`flag of ${entry.country.name}`}
+                            title={entry.country.name}
+                        />
+                        </span>
+                        <span>{(() => {
+                            const sliced = entry.country.name.slice(0, 12)
+                            return sliced.length === 12 ? sliced + "..." : sliced
+                        })()}</span>
+                    </td>
+                    <td className="leaderboard-tiles">{entry.tiles}</td>
+                </tr>
             })}
             </tbody>
-        </table>
+        </table>}
+
     </div>
 }
