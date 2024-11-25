b333fe9b1cc6352c0e5aaa7428074664d2b51fc7
djoudimansouri-ti <djoudi.mansouri@tech-instinct.com>
2024-11-25 03:33:40 +0100

    improved Leaderboard ui, in addition to adding QoL improves like : autorotate when fully zoomed out, mouse rotation speed proportionally decreases the more you zoom in, and made the mouse rotation effect smoother by adding damping

--- src/app/Leaderboard.tsx
+++ src/app/Leaderboard.tsx
@@ -10,26 +10,29 @@ type LeaderboardProps = {
 }
 
 export default function Leaderboard(props: LeaderboardProps) {
-    return <div className="leaderboard">
+    return <div className="leaderboard clean-scrollbar">
         <h2 className="leaderboard-title">Leaderboard</h2>
         <table>
             <tbody>
             {props.data.map((entry, index) => {
                 return <tr key={index} className="leaderboard-entry">
-                    <td>{index + 1}</td>
-                    <td>{(() => {
-                        const sliced = entry.country.name.slice(0, 20)
-                        return sliced.length === 20 ? sliced + "..." : sliced
-                    })()}</td>
-                    <td><img
-                        className="leaderboard-entry-flag"
-                        src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
-                        alt={`flag of ${entry.country.name}`}
-                        title={entry.country.name}
-                    />
-                    </td>
-                    <td>{entry.tiles}</td>
-                </tr>
+                            <div>
+                                <td>{index + 1}</td>
+                                <td><img
+                                    className="leaderboard-entry-flag"
+                                    src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
+                                    alt={`flag of ${entry.country.name}`}
+                                    title={entry.country.name}
+                                />
+                                </td>
+                                <td>{(() => {
+                                    const sliced = entry.country.name.slice(0, 20)
+                                    return sliced.length === 20 ? sliced + "..." : sliced
+                                })()}</td>
+                            </div>
+
+                            <td className="leaderboard-tiles">{entry.tiles}</td>
+                        </tr>
             })}
             </tbody>
         </table>
