56de924c051cc6540f4cdff2f42f5d77266f12df
Arnakuzy <arnakuzy@gmail.com>
2024-12-04 21:51:21 +0100

    up: UI update on the pannels

--- src/app/Leaderboard.tsx
+++ src/app/Leaderboard.tsx
@@ -1,6 +1,6 @@
 import "./Leaderboard.css"
-import {Country} from "./countries.ts";
-import {useState} from "react";
+import { Country } from "./countries.ts";
+import { useState } from "react";
 
 
 type LeaderboardProps = {
@@ -22,43 +22,44 @@ export default function Leaderboard(props: LeaderboardProps) {
             <h2 className="leaderboard-title">Leaderboard</h2>
             <p className="leaderboard-caption">(click to {isOpen ? "collapse" : "expand"})</p>
         </div>
-        {isOpen && <table className="leaderboard-table">
-            <thead>
-            <tr>
-                <th>Country</th>
-                <th>% owned</th>
-                <th>Tiles</th>
-            </tr>
-            </thead>
+        {isOpen &&
+            <div className="leaderboard-table-container">
+                <table className="leaderboard-table">
+                    <thead>
+                        <tr>
+                            <th colSpan={3} >Country</th>
+                            <th>%</th>
+                            <th>Tiles</th>
+                        </tr>
+                    </thead>
 
-            <tbody>
-            {props.data.map((entry, index) => {
-                return <tr key={index} className="leaderboard-entry">
-                    <td>
-                        <span>{index + 1}</span>
-                        <span><img
-                            className="leaderboard-entry-flag"
-                            src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
-                            alt={`flag of ${entry.country.name}`}
-                            title={entry.country.name}
-                        />
-                        </span>
-                        <span className="leaderboard-entry-countryname">{(() => {
-                            const sliced = entry.country.name.slice(0, 20)
-                            return sliced.length === 20 ? sliced + "..." : sliced
-                        })()}</span>
-
-                    </td>
-                    <td>
-                        <span className="leaderboard-entry-percentage">
-                            {(entry.tiles / props.tilesCount * 100).toFixed(2)}%
-                        </span>
-                    </td>
-                    <td className="leaderboard-tiles">{entry.tiles}</td>
-                </tr>
-            })}
-            </tbody>
-        </table>}
-
-    </div>
+                    <tbody>
+                        {props.data.map((entry, index) => {
+                            return <tr key={index} className="leaderboard-entry">
+                                <td className="leaderboard-entry-index" >{index + 1}.</td>
+                                <td className="leaderboard-entry-flag-container"><img
+                                    className="leaderboard-entry-flag"
+                                    src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
+                                    alt={`flag of ${entry.country.name}`}
+                                    title={entry.country.name}
+                                />
+                                </td>
+                                <td className="leaderboard-entry-countryname">
+                                    {(() => {
+                                        const max_length = 17;
+                                        const sliced = entry.country.name.slice(0, max_length)
+                                        return sliced.length === max_length ? sliced.trim() + "..." : sliced
+                                    })()}
+                                </td>
+                                <td className="leaderboard-entry-percentage">
+                                    {(entry.tiles / props.tilesCount * 100).toFixed(2)}%
+                                </td>
+                                <td className="leaderboard-tiles">{entry.tiles}</td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        }
+    </div >
 }
