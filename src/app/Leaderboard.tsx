4b5fe80d9191af506380b6fc2bd55649f76da8e3
Albertitux <alberto.garvarez@gmail.com>
2024-12-12 01:36:02 +0100

    side panel: new ui

--- src/app/Leaderboard.tsx
+++ src/app/Leaderboard.tsx
@@ -17,19 +17,26 @@ export default function Leaderboard(props: LeaderboardProps) {
         setIsOpen(!isOpen)
     }
 
-    return <div className="leaderboard clean-scrollbar">
-        <div className="leaderboard-header" onClick={toggleLeaderboard}>
-            <h2 className="leaderboard-title">Leaderboard</h2>
-            <p className="leaderboard-caption">(click to {isOpen ? "collapse" : "expand"})</p>
+    return <div className="leaderboard">
+        <div className="leaderboard-header">
+            <img alt="ClickPlanet logo"
+                 src="/static/logo.svg"
+                 width="56px"
+                 height="56px"/>
+            <h1>ClickPlanet</h1>
+        </div>
+        <div className="leaderboard-expand">
+            <button className="button button-mini" onClick={toggleLeaderboard}>{isOpen ? "Close" : "Champions"}</button>
         </div>
         {isOpen &&
             <div className="leaderboard-table-container">
                 <table className="leaderboard-table">
                     <thead>
                         <tr>
-                            <th colSpan={3} >Country</th>
-                            <th>%</th>
-                            <th>Tiles</th>
+                            <th></th>
+                            <th colSpan={3}>🏆</th>
+                            <th className="leaderboard-table-number leaderboard-table-tiles">⚪️</th>
+                            <th className="leaderboard-table-number">%</th>
                         </tr>
                     </thead>
 
@@ -37,24 +44,17 @@ export default function Leaderboard(props: LeaderboardProps) {
                         {props.data.map((entry, index) => {
                             return <tr key={index} className="leaderboard-entry">
                                 <td className="leaderboard-entry-index" >{index + 1}.</td>
-                                <td className="leaderboard-entry-flag-container"><img
-                                    className="leaderboard-entry-flag"
-                                    src={`static/countries/svg/${entry.country.code.toLowerCase()}.svg`}
-                                    alt={`flag of ${entry.country.name}`}
-                                    title={entry.country.name}
-                                />
-                                </td>
-                                <td className="leaderboard-entry-countryname">
+                                <td colSpan={3}>
                                     {(() => {
                                         const max_length = 17;
                                         const sliced = entry.country.name.slice(0, max_length)
-                                        return sliced.length === max_length ? sliced.trim() + "..." : sliced
+                                        return sliced.length === max_length ? sliced.trim() + "." : sliced
                                     })()}
                                 </td>
-                                <td className="leaderboard-entry-percentage">
-                                    {(entry.tiles / props.tilesCount * 100).toFixed(2)}%
+                                <td className="leaderboard-table-number leaderboard-table-tiles">{entry.tiles}</td>
+                                <td className="leaderboard-table-number">
+                                    {(entry.tiles / props.tilesCount * 100).toFixed(2)}
                                 </td>
-                                <td className="leaderboard-tiles">{entry.tiles}</td>
                             </tr>
                         })}
                     </tbody>
--- src/app/components/DiscordButton.tsx
+++ src/app/components/DiscordButton.tsx
@@ -4,23 +4,16 @@ export type DiscordButtonProps = {
     message?: string;
 };
 
-export default function DiscordButton({message = "Join our Discord server"}: DiscordButtonProps) {
+export default function DiscordButton({message = "Join us on Discord"}: DiscordButtonProps) {
     return (
-        <div>
             <a
                 href="https://discord.gg/Nwekj6ndbn"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="discord-button-link"
+                className="button button-discord"
                 aria-label={message}
             >
-                <img
-                    src="/static/discord.svg"
-                    className="discord-button-img"
-                    alt="Discord logo"
-                />
-                <span className="discord-button-text">{message}</span>
+                {message}
             </a>
-        </div>
     );
 }
